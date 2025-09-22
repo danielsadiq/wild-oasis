724369689ee945c9d23535f5c0016c68b4b7e9c5
Daniel Sadiq <danielsadiq93@gmail.com>
2025-09-22 10:56:03 +0100

    api update

--- src/services/apiCabins.ts
+++ src/services/apiCabins.ts
@@ -1,14 +1,20 @@
-import { supabase } from "./supabase"
+import type { CabinType } from "../types/cabinType";
+import { supabase } from "./supabase";
 
-export async function getCabins(){
-  
-const{ data, error } = await supabase
-  .from('cabins')
-  .select('*')
-  if (error){
-    console.error(error)
+export async function getCabins(): Promise<CabinType[]> {
+  const { data, error } = await supabase.from("cabins").select("*");
+  if (error) {
+    console.error(error);
     throw new Error("Cabins could not be loaded");
   }
 
-  return data
+  return data;
+}
+
+export async function deletCabins(id: number) {
+  const { error } = await supabase.from("cabins").delete().eq("id", id);
+  if (error) {
+    console.error(error);
+    throw new Error("cabin could not be deleted");
+  }
 }
--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -2,5 +2,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://jkfizxdgzrtrlsaarzqw.supabase.co'
 // const supabaseKey = process.env.SUPABASE_KEY
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
-console.log(supabaseKey)
 export const supabase = createClient(supabaseUrl, supabaseKey)
