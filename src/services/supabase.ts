7451ee0fbfcf8f57b4cb9a92d2e1106da5900b21
Daniel Sadiq <danielsadiq93@gmail.com>
2025-09-23 22:16:54 +0100

    updates

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://jkfizxdgzrtrlsaarzqw.supabase.co'
+export const supabaseUrl = 'https://jkfizxdgzrtrlsaarzqw.supabase.co'
 // const supabaseKey = process.env.SUPABASE_KEY
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey)
